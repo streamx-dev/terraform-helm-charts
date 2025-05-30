ccd7da11061fe217314373823bb142d25d39e6ba
Kamil Chociej <kamil.chociej@ds.pl>
2025-05-30 12:17:00 +0200

    [DXP-1983] remove unused kubectl provider requirement

--- modules/ingress-controller-apisix/versions.tf
+++ modules/ingress-controller-apisix/versions.tf
@@ -24,9 +24,5 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.17.0"
     }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.19.0"
-    }
   }
 }
